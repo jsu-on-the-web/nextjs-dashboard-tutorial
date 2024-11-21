19e2d429eea71649514c8b77a1a00165077c0764
jsu-on-the-web <janet.suryadi@nearmap.com>
2024-11-22 09:11:30 +1100

    Up to chapter 6

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const DashboardPage: React.FC = () => {
+    return <p>Dashboard Page</p>;
+};
+
+export default DashboardPage;
--- app/ui/dashboard/page.tsx
+++
@@ -1,5 +0,0 @@
-export const Page = () => { 
-    return <p>Dashboard Page</p>;
-};
-
-export default Page;
