795a34ab4d868f699adfe88db9d03593e68c6e6c
jsu-on-the-web <janet.suryadi@nearmap.com>
2024-11-22 16:17:23 +1100

    Rename pages

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,6 +1,5 @@
-'use client';
 import React from 'react';
 
-export default function Page() {
+export default function CustomersPage() {
     return <p>Customers Page</p>;
 }
--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,6 +1,5 @@
-'use client';
 import React from 'react';
 
-export default function Page() {
+export default function InvoicesPage() {
     return <p>Invoices Page</p>;
 }
